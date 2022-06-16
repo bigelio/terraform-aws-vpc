effefc930d30c1a5904e2a03504c32ae95ad1225
Gelio <geliobizimana01@gmail.com>
2022-06-16 22:02:05 +0200

    Create main.tf with basic vpc configs

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+
+provider "aws" {
+  region = "af-south-1"
+}
+
+# section for creating a VPC
+
+resource "aws_vpc" "custom-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "custom-vpc"
+  }
+}
